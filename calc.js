c0909276940fd0db7212475ae3829f0bd0d7f3d5
Sierra <sierramroberts.96@gmail.com>
2017-02-15 14:58:37 -0500

    calculator

---
+++ calc.js
@@ -0,0 +1,42 @@
+
+
+var answer = 0;
+
+// Function Definitions
+
+// 1. Write a function called add that takes two arguments and adds them together, returning the result
+
+function add(n1,n2) {
+   return n1 + n2
+}
+
+// 2. Write a function called subtract that takes two arguments and subtracts them from each other, returning the result
+
+function subtract(n1,n2) {
+    return n1 - n2
+}
+
+// 3. Write a function called multiply that takes two arguments and multiplies them other, returning the result
+
+function multiply(n1,n2) {
+    return n1 * n2
+}
+
+// 4. Write a function called divide that takes two arguments and divides them from each other, returning the result
+
+function divide(n1,n2) {
+    return n1 % n2
+}
+
+
+
+// Event Handlers
+document.querySelector('#calc').addEventListener('click', function(e) {
+    var calcFunction = e.target.id,
+        number1 = Number(document.querySelector('#number1').value),
+        number2 = Number(document.querySelector('#number2').value);
+
+    answer = window[calcFunction](number1, number2);
+
+    document.querySelector('#answer').innerHTML = answer;
+});
